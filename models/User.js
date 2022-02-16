fb54babead9db03ecd948c1ca56fdbb05991b00f
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-15 18:26:56 -0600

    added login componenet

--- models/User.js
+++ models/User.js
@@ -3,13 +3,12 @@ const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
 
 // create our User model
-class User extends Model {}
-
-
-// // add bcrypt function per documentation
-// bcrypt.hash(myPlaintextPassword, saltRounds).then(function(hash){
-//     //store hash in your password db
-// });
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
