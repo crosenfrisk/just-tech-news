d35701e2fb3031b887fc42936df592bca035e08f
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-22 15:06:50 -0600

    add view/layouts/ folders to root directory and main.handlebars and homepage.handlebars inside

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection');
 const path = require('path');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
