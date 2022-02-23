9c2428cf796661d91f7fa44e6672a716cca1b386
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-23 09:36:08 -0600

    added const response to hold the result of the promise with await fetch()

--- public/stylesheets/javascript/login.js
+++ public/stylesheets/javascript/login.js
@@ -1,22 +1,21 @@
 async function signupFormHandler(event) {
   event.preventDefault();
 
-  const username = document.querySelector("#username-signup").value.trim();
-  const email = document.querySelector("#email-signup").value.trim();
-  const password = document.querySelector("#password-signup").value.trim();
+  const username = document.querySelector('#username-signup').value.trim();
+  const email = document.querySelector('#email-signup').value.trim();
+  const password = document.querySelector('#password-signup').value.trim();
 
   if (username && email && password) {
-    await fetch("/api/users", {
-      method: "post",
+    const response = await fetch('/api/users', {
+      method: 'post',
       body: JSON.stringify({
         username,
         email,
-        password,
+        password
       }),
-      headers: { "Content-Type": "application/json" },
-    }).then((response) => {
-      console.log(response);
+      headers: { 'Content-Type': 'application/json' }
     });
+    console.log(response);
   }
 }
 
