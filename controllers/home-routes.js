4ba643c2f41aea49f206856ef7e7932467b0b204
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-23 09:51:25 -0600

    update router.get() for home-routes.js /login

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
+// get all posts for homepage
 router.get('/', (req, res) => {
   Post.findAll({
     attributes: [
@@ -38,6 +39,11 @@ router.get('/', (req, res) => {
 });
 
 router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
   res.render('login');
 });
 
