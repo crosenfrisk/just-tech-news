c3687f09a09cb6fdaf81ad3698acaa38777d143e
crosenfrisk <82235272+crosenfrisk@users.noreply.github.com>
2022-02-26 14:09:05 -0600

    Protecting the routes with middleware function withAuth. Inserted into POST, PUT, and DELETE routes.

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
+const withAuth = require('../utils/auth');
 
-router.get('/', (req, res) => {
+router.get('/', withAuth, (req, res) => {
     Post.findAll({
         where: {
             // use the ID from the session
@@ -33,7 +34,7 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
         // serialize data before passing to template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('dashboard', { loggedIn: true });
+        res.render('dashboard', { posts, loggedIn: true });
     })
     .catch(err => {
         console.log(err);
